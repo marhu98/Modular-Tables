219bb5b726d148ff9fb40539f711dd78fd64d2e8
marhu98 <marioytambor@gmail.com>
2021-04-24 23:35:14 +0200

    commit

--- gifphy.py
+++ gifphy.py
@@ -1,11 +1,14 @@
-import glob
-from PIL import Image
-
+from glob import glob
+import imageio
 # filepaths
-fp_in = "frames/1/png/frame*.png"
+fp_in = "frames/1/png/*.png"
 fp_out = "gifs/1.gif"
 
-# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
-img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
-img.save(fp=fp_out, format='GIF', append_images=imgs,
-         save_all=True, duration=200, loop=0)
+filenames= sorted(glob(fp_in))
+
+images = []
+
+for file in filenames:
+    images.append(imageio.imread(file))
+
+imageio.mimsave(fp_out,images)

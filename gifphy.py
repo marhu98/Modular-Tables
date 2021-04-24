fb49ad82ce55e0fb2b459c4b52f8778b33013da2
marhu98 <marioytambor@gmail.com>
2021-04-24 23:42:34 +0200

    commit

--- gifphy.py
+++ gifphy.py
@@ -1,10 +1,16 @@
 from glob import glob
+import re
 import imageio
+
+def index(value):
+    return int(re.search("frame(?P<number>\d*)\.png",value).groupdict()["number"])
+
+
 # filepaths
 fp_in = "frames/1/png/*.png"
 fp_out = "gifs/1.gif"
 
-filenames= sorted(glob(fp_in))
+filenames= sorted(glob(fp_in),key=index)
 
 images = []
 
